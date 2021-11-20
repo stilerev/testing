6c7a162a141e8c72b41216d2bd25f3205f6f1fa3
stilerev <ericstefansson13@outlook.com>
2021-11-21 00:08:10 +0100

    some msg

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const http = require("http");
 const express = require("express");
 const app = express();
 
-const PORT = 3000;
+const PORT = 80;
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
